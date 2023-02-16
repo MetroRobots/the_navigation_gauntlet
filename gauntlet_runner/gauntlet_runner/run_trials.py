6547068d09e85c0a26f597f3546defed41071b7e
David V. Lu <davidvlu@gmail.com>
2023-02-16 16:43:24 -0500

    Write parameter files

--- gauntlet_runner/gauntlet_runner/run_trials.py
+++ gauntlet_runner/gauntlet_runner/run_trials.py
@@ -1,7 +1,15 @@
 import argparse
+from ament_index_python.packages import get_package_share_path
 from ros2launch.api import get_share_file_path_from_package, launch_a_launch_file
 import tempfile
 import yaml
+
+
+def write_temp_parameter_file(parameters, node_name='/**'):
+    temp_config_path = tempfile.NamedTemporaryFile()
+    yaml_data = {node_name: {'ros__parameters': parameters}}
+    yaml.safe_dump(yaml_data, open(temp_config_path.name, 'w'))
+    return temp_config_path
 
 
 def main():
@@ -9,6 +17,14 @@ def main():
     parser.add_argument('simulator_package')
     parser.add_argument('nav_config_package')
     args = parser.parse_args()
+
+    record_config = {'topics': []}
+
+    nav_pkg_path = get_package_share_path(args.nav_config_package)
+    config_path = nav_pkg_path / 'config/nav_gauntlet.yaml'
+    if config_path.exists():
+        config = yaml.safe_load(open(config_path))
+        record_config['topics'] += config.get('topics', [])
 
     trial_launch = get_share_file_path_from_package(
         package_name='gauntlet_runner',
@@ -18,9 +34,11 @@ def main():
     launch_arguments.append(f'nav_config_package:={args.nav_config_package}')
 
     trial_config = {'goal_pose_x': 2.0}
-    trial_config_path = tempfile.NamedTemporaryFile()
-    yaml.safe_dump({'trial_runner': {'ros__parameters': trial_config}}, open(trial_config_path.name, 'w'))
+    trial_config_path = write_temp_parameter_file(trial_config)
     launch_arguments.append(f'trial_config_path:={trial_config_path.name}')
+
+    record_config_path = write_temp_parameter_file(record_config)
+    launch_arguments.append(f'record_config_path:={record_config_path.name}')
 
     launch_a_launch_file(
         launch_file_path=trial_launch,
