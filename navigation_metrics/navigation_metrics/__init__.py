a989c5f3c9538a1e96c5316ae35064a455213cb9
David V. Lu <davidvlu@gmail.com>
2023-02-17 17:10:25 -0500

    Velocity stats

--- navigation_metrics/navigation_metrics/__init__.py
+++ navigation_metrics/navigation_metrics/__init__.py
@@ -1,5 +1,6 @@
 from .basic import *  # noqa: F401, F403
 from .path import *  # noqa: F401, F403
+from .velocity import *  # noqa: F401, F403
 from .metric import RecordedMessage, get_metrics, get_conversion_functions
 
 __all__ = ['RecordedMessage', 'get_metrics', 'get_conversion_functions']
--- navigation_metrics/navigation_metrics/util.py
+++ navigation_metrics/navigation_metrics/util.py
@@ -1,4 +1,5 @@
 from math import hypot
+from angles import shortest_angular_distance
 
 
 def point_distance(p0, p1):
@@ -14,3 +15,31 @@ def pose_distance(p0, p1):
 
 def pose_stamped_distance(p0, p1):
     return point_distance(p0.pose.position, p1.pose.position)
+
+
+def pose2d_distance(p0, p1):
+    dx = p0.x - p1.x
+    dy = p0.y - p1.y
+    return hypot(dx, dy), shortest_angular_distance(p0.theta, p1.theta)
+
+
+def stamp_to_float(stamp):
+    return stamp.sec + stamp.nanosec / 1e9
+
+
+def average(series, getter):
+    total = 0.0
+    count = 0
+    for rmsg in series:
+        total += getter(rmsg)
+        count += 1
+    return total / count
+
+
+def metric_max(series, getter):
+    best = None
+    for rmsg in series:
+        v = getter(rmsg)
+        if best is None or v > best:
+            best = v
+    return best
