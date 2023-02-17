ec4b2e39c694c4ee955e78ea96c042899dc124ae
David V. Lu <davidvlu@gmail.com>
2023-02-17 13:04:23 -0500

    Connection Map

--- gauntlet_analysis/gauntlet_analysis/trial_bag.py
+++ gauntlet_analysis/gauntlet_analysis/trial_bag.py
@@ -6,7 +6,8 @@ import pathlib
 
 types_to_register = {}
 for type_name in ['action_msgs/msg/GoalStatus',
-                  'action_msgs/msg/GoalInfo']:
+                  'action_msgs/msg/GoalInfo',
+                  'nav_2d_msgs/msg/Pose2DStamped']:
     i_path = pathlib.Path(get_interface_path(type_name))
     msg_text = i_path.read_text()
     types_to_register.update(get_types_from_msg(msg_text, type_name))
@@ -22,21 +23,21 @@ class TrialBag:
         self.bag_reader = Reader(str(path))
         self.bag_reader.open()
 
+        self.connection_map = {conn.topic: conn for conn in self.bag_reader.connections.values()}
+
     def __del__(self):
         if self.bag_reader:
             self.bag_reader.close()
 
-    def get_topic(self, topic):
+    def __getitem__(self, topic):
         if topic in self.cached_topics:
             return self.cached_topics[topic]
 
         self.cached_topics[topic] = []
 
-        connections = [conn for conn in self.bag_reader.connections.values() if conn.topic == topic]
-
-        for connection, timestamp, rawdata in self.bag_reader.messages(connections=connections):
+        for conn, timestamp, rawdata in self.bag_reader.messages(connections=[self.connection_map[topic]]):
             ts = timestamp / 1e9
-            self.cached_topics[topic].append((ts, deserialize_cdr(rawdata, connection.msgtype)))
+            self.cached_topics[topic].append((ts, deserialize_cdr(rawdata, conn.msgtype)))
         return self.cached_topics[topic]
 
     def __repr__(self):
--- navigation_metrics/navigation_metrics/basic.py
+++ navigation_metrics/navigation_metrics/basic.py
@@ -4,8 +4,22 @@ from action_msgs.msg import GoalStatus
 
 @nav_metric
 def completed(data):
-    results = data.get_topic('/navigation_result')
+    results = data['/navigation_result']
     if not results:
         return False
     _, result = results[0]
     return result.status == GoalStatus.STATUS_SUCCEEDED
+
+
+@nav_metric
+def total_time(data):
+    goals = data['/trial_goal_pose']
+    results = data['/navigation_result']
+
+    if not goals or not results:
+        return
+
+    t0 = goals[0][0]
+    t1 = results[0][0]
+
+    return t1 - t0
