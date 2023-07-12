1aa99398969ca7438aa884708732c9a657974957
David V. Lu <davidvlu@gmail.com>
2023-07-12 14:11:02 -0400

    Ignore errors

--- gauntlet_analysis/gauntlet_analysis/analyze_bag.py
+++ gauntlet_analysis/gauntlet_analysis/analyze_bag.py
@@ -7,13 +7,20 @@ from navigation_metrics import get_metrics, global_metric_search
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('bag_path', type=pathlib.Path)
+    parser.add_argument('-i', '--ignore-errors', action='store_true')
     args = parser.parse_args()
 
     global_metric_search()
 
     bag = TrialBag(args.bag_path)
     for name, metric in get_metrics().items():
-        m = metric(bag)
+        try:
+            m = metric(bag)
+        except Exception:
+            if args.ignore_errors:
+                continue
+            else:
+                raise
         print(name, m)
 
 
