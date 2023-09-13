00099a220a9c476e74a5404b7e5b686bbe9f823a
David V. Lu <davidvlu@gmail.com>
2023-09-13 14:25:27 -0400

    Another dimension

--- navigation_metrics/navigation_metrics/plot.py
+++ navigation_metrics/navigation_metrics/plot.py
@@ -25,19 +25,26 @@ def main():
     parser.add_argument('y')
     parser.add_argument('folder', type=pathlib.Path, default='.', nargs='?')
     parser.add_argument('-p', '--plots-axis')
+    parser.add_argument('-s', '--series-axis')
     args = parser.parse_args()
 
     data = analyze_bags(args.folder, ComputeMode.NOTHING)
 
-    xs = collections.defaultdict(list)
-    ys = collections.defaultdict(list)
+    xs = collections.defaultdict(lambda: collections.defaultdict(list))
+    ys = collections.defaultdict(lambda: collections.defaultdict(list))
     counts = collections.Counter()
+    counts[args.x] = 0  # Ensures values are in counter for debug printing
+    counts[args.y] = 0
 
     for path, metrics in sorted(data.items()):
         x_v = get_value(metrics, args.x)
         y_v = get_value(metrics, args.y)
         if args.plots_axis:
-            s_v = get_value(metrics, args.plots_axis, allow_str=True)
+            p_v = get_value(metrics, args.plots_axis, allow_str=True)
+        else:
+            p_v = None
+        if args.series_axis:
+            s_v = get_value(metrics, args.series_axis, allow_str=True)
         else:
             s_v = None
 
@@ -45,12 +52,14 @@ def main():
             counts[args.x] += 1
         if y_v:
             counts[args.y] += 1
-        if s_v:
+        if p_v:
             counts[args.plots_axis] += 1
+        if s_v:
+            counts[args.series_axis] += 1
 
         if x_v is not None and y_v is not None:
-            xs[s_v].append(x_v)
-            ys[s_v].append(y_v)
+            xs[p_v][s_v].append(x_v)
+            ys[p_v][s_v].append(y_v)
 
     for dimension, count in counts.most_common():
         click.secho(f'Dimension "{dimension}" found in {count}/{len(data)} bags', fg='blue' if count else 'red')
@@ -65,10 +74,19 @@ def main():
     else:
         axes = ax_v
 
-    for s_v, ax in zip(xs.keys(), axes):
+    for p_v, ax in zip(xs.keys(), axes):
         if args.plots_axis:
-            ax.set_title(f'{args.plots_axis} = {s_v}')
-        ax.plot(xs[s_v], ys[s_v], 'o')
+            ax.set_title(f'{args.plots_axis} = {p_v}')
+
+        for s_v in xs[p_v]:
+            if args.series_axis:
+                label = f'{args.series_axis} = {s_v}'
+            else:
+                label = None
+
+            ax.plot(xs[p_v][s_v], ys[p_v][s_v], 'o', label=label)
+        if args.series_axis:
+            ax.legend()
         ax.set_xlabel(args.x)
         ax.set_ylabel(args.y)
     show()
