cb2999fc9b33597ee6798bfb28fc86c08c1cf922
David V. Lu <davidvlu@gmail.com>
2023-09-15 13:14:38 -0400

    Update Viz Trial

--- navigation_metrics_viz/navigation_metrics_viz/viz_trial.py
+++ navigation_metrics_viz/navigation_metrics_viz/viz_trial.py
@@ -3,12 +3,14 @@ from matplotlib.pyplot import subplots, show
 import pathlib
 from math import hypot, atan2
 
-from navigation_metrics import FlexibleBag, global_metric_search
+from navigation_metrics import FlexibleBag, global_metric_search, MissingTopicException
+from navigation_metrics_viz.plot_bag import Datum
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('bagfile', type=pathlib.Path)
+    parser.add_argument('dimension', nargs='*')
     args = parser.parse_args()
 
     global_metric_search()
@@ -37,21 +39,40 @@ def main():
     pys = []
     psizes = []
     pcolors = []
-    for ped_rmsg, path_rmsg in bag['/pedestrians', '/path2d']:
-        p0 = path_rmsg.msg.pose
-        for pedestrian in ped_rmsg.msg.pedestrians:
-            p1 = pedestrian.pose
-            dx = p1.x - p0.x
-            dy = p1.y - p0.y
-            d = hypot(dx, dy)
-            angle = atan2(dy, dx) - p0.theta
-            pxs.append(ped_rmsg.t - t0)
-            pys.append(angle)
-            s = 10 / d
-            psizes.append(s*s)
-            pcolors.append(hypot(pedestrian.velocity.x, pedestrian.velocity.y))
+    try:
+        for ped_rmsg, path_rmsg in bag['/pedestrians', '/path2d']:
+            p0 = path_rmsg.msg.pose
+            for pedestrian in ped_rmsg.msg.pedestrians:
+                p1 = pedestrian.pose
+                dx = p1.x - p0.x
+                dy = p1.y - p0.y
+                d = hypot(dx, dy)
+                angle = atan2(dy, dx) - p0.theta
+                pxs.append(ped_rmsg.t - t0)
+                pys.append(angle)
+                s = 10 / d
+                psizes.append(s*s)
+                pcolors.append(
+                    hypot(pedestrian.velocity.x, pedestrian.velocity.y))
+    except MissingTopicException:
+        pass
 
     ax.scatter(pxs, pys, s=psizes, c=pcolors, cmap='summer')
+
+    exs = []
+    eys = []
+    if '/scout_base/estop' in bag:
+        for t, msg in bag['/scout_base/estop']:
+            exs.append(t - t0)
+            eys.append(0)
+    ax.scatter(exs, eys, marker='x', color='red')
+
+    for dimension in args.dimension:
+        d = Datum(dimension)
+        t = Datum(d.topic + '.t')
+        t_s = t.get_sequence(bag)
+        x_s = d.get_sequence(bag)
+        ax.plot(t_s, x_s, label=dimension)
 
     show()
 
