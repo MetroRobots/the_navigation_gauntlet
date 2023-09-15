a4032cab6d382ff6727ae1c39992ad2437c94531
David V. Lu <davidvlu@gmail.com>
2023-09-15 13:13:43 -0400

    Remove Result Hack

--- navigation_metrics/navigation_metrics/metrics/basic.py
+++ navigation_metrics/navigation_metrics/metrics/basic.py
@@ -2,7 +2,6 @@ from action_msgs.msg import GoalStatus
 
 from navigation_metrics.metric import nav_metric
 from navigation_metrics.flexible_bag import BagMessage, flexible_bag_converter_function
-from navigation_metrics.util import point_distance
 
 
 @flexible_bag_converter_function('/trial_goal_pose')
@@ -12,44 +11,6 @@ def convert_to_trial(data):
         start_bmsg = goal_pose_msgs[0]
         seq = [BagMessage(start_bmsg.t, start_bmsg.msg)]
         return seq
-
-
-@flexible_bag_converter_function('/navigation_result')
-def find_endpoint(data):
-    goal_pose_msgs = data['/trial_goal_pose']
-    cmds = data['/cmd_vel']
-    if not goal_pose_msgs or not cmds:
-        return
-    goal = goal_pose_msgs[0].msg
-
-    # Find last active cmd_vel
-    i = len(cmds) - 1
-    while i >= 0:
-        t, msg = cmds[i]
-        if abs(msg.linear.x) < 1e-2 and abs(msg.angular.z) < 1e-2:
-            i -= 1
-        else:
-            break
-
-    last_cmd_t = cmds[i + 1].t
-
-    global_frame = data.get_parameter('global_frame', 'map')
-    robot_frame = data.get_parameter('robot_frame', 'base_link')
-    distance_tolerance = data.get_parameter('distance_tolerance', 0.5)
-
-    for t, msg in data.get_messages_by_time('/tf', last_cmd_t):
-        for transform in msg.transforms:
-            if transform.header.frame_id == global_frame and transform.child_frame_id == robot_frame:
-                d = point_distance(goal.pose.position, transform.transform.translation)
-
-                new_msg = GoalStatus()
-                if d < distance_tolerance:
-                    new_msg.status = GoalStatus.STATUS_SUCCEEDED
-                else:
-                    new_msg.status = GoalStatus.STATUS_ABORTED
-
-                seq = [BagMessage(t, new_msg)]
-                return seq
 
 
 @nav_metric
